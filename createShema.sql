961d8be145cccfadf277ee808a51a6cdfe63d3df
urielSanchezMxGto <153027435+urielSanchezMxGto@users.noreply.github.com>
2026-06-06 16:51:36 -0600

    Update createShema.sql
    cambio de cursojava a CURSOJAVA

--- createShema.sql
+++ createShema.sql
@@ -1,18 +1,18 @@
 -- Crear el usuario
-CREATE USER cursojava IDENTIFIED BY cursojava
+CREATE USER CURSOJAVA IDENTIFIED BY cursojava
 DEFAULT TABLESPACE users
 TEMPORARY TABLESPACE temp
 QUOTA UNLIMITED ON users;
 
 -- Dar privilegios básicos para conectarse y trabajar con sus tablas
-GRANT CREATE SESSION TO cursojava;  -- Permiso para hacer login
-GRANT CREATE TABLE TO cursojava; --Permiso para crear tablas
-GRANT CREATE SEQUENCE TO cursojava; -- Permiso para crear secuencias
-GRANT CREATE VIEW TO cursojava; -- Permiso para crear vistas
-GRANT CREATE TRIGGER TO cursojava;  -- Permiso para crear triggers
-GRANT CREATE PROCEDURE TO cursojava; -- Permiso para crear funciones y procedimientos almacenados
-GRANT CREATE MATERIALIZED VIEW TO cursojava; -- Permiso para crear vistas materializadas
-GRANT CREATE DATABASE LINK TO cursojava; -- Permiso para crear database link
+GRANT CREATE SESSION TO CURSOJAVA;  -- Permiso para hacer login
+GRANT CREATE TABLE TO CURSOJAVA; --Permiso para crear tablas
+GRANT CREATE SEQUENCE TO CURSOJAVA; -- Permiso para crear secuencias
+GRANT CREATE VIEW TO CURSOJAVA; -- Permiso para crear vistas
+GRANT CREATE TRIGGER TO CURSOJAVA;  -- Permiso para crear triggers
+GRANT CREATE PROCEDURE TO CURSOJAVA; -- Permiso para crear funciones y procedimientos almacenados
+GRANT CREATE MATERIALIZED VIEW TO CURSOJAVA; -- Permiso para crear vistas materializadas
+GRANT CREATE DATABASE LINK TO CURSOJAVA; -- Permiso para crear database link
 
 -- Rol resource
-GRANT RESOURCE TO cursojava;
+GRANT RESOURCE TO CURSOJAVA;
